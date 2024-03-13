8f3abd448bedcb68187ddf628e4dfe5382509d3c
Franklin Barto <franklinbarto@Users-iMac-3.local>
2024-03-13 15:20:54 +0300

    gltf model added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import { OrbitControls, Torus } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 import Box from "./components/box";
 
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { useLoader } from '@react-three/fiber';
+
+import bot from './assets/Xbot.glb'
+
+import { useAnimations } from "@react-three/drei"
+
+function Model() {
+  
+  const model = useLoader(GLTFLoader, bot);
+  // Extract animation actions
+  const { ref, actions, names } = useAnimations(model.animations)
+  const [index, setIndex] = useState(4)
+  
+  return <primitive object={model.scene} onClick={() => setIndex((index + 1) % names.length)}/>;
+}
+
 const App = () => {
+
   return (
     <Canvas>
       <OrbitControls />
       <Suspense>
-        <Physics debug gravity={[0,-9,0]}>
-
+        <Physics debug gravity={[0, -9, 0]}>
+          
           <RigidBody position={[0, 3, 0]}>
             <Box/>
+          </RigidBody>
+          
+          <RigidBody position={[0, -2, 0]} >
+            <Model/>
           </RigidBody>
           
           <RigidBody colliders={"hull"} restitution={2}>
